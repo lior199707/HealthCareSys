741f8161098fb84bff5e00a9b6dc5aa39dd1d496
Evgeni <bursov1995@gmail.com>
2021-11-23 22:06:14 +0200

    doctorRegister C

--- Project1/DoctorFunctions.c
+++ Project1/DoctorFunctions.c
@@ -1,5 +1,28 @@
 #include "DoctorFunctions.h"
 #include "MutualFunctions.h"
+//gets all the information of a doctor and adds it to the data base
+void doctorRegister()
+{
+	char* fullName = getName();//gets the name
+	char* id = getId();//gets the id
+	char* password = getPass();//gets the password 
+	char gender = getGender();//gets the gender
+	char* title = getTitle();//gets the title
+	char* medicalField = getMedicalField();//gets the medical field
+	char* listOfHours = getListOfTurns();//gets the list of appointment hours
+	addDoctorToDb(id, password, fullName, medicalField, title, gender, listOfHours);//adds all the information to the db
+	free(fullName);
+	free(id);
+	free(password);
+	free(title);
+	free(medicalField);
+	free(listOfHours);
+}
+
+
+
+
+
 
 //printf("shalom");
 //jklkjlkjlkjlk
