dd3731b530ad86a6857a17fd2df0f43f9a2a3279
noybuskila <noybu2@ac.sce.ac.il>
2021-11-23 21:46:11 +0200

    clientRegister.h

--- Project1/ClientFunctions.h
+++ Project1/ClientFunctions.h
@@ -2,5 +2,7 @@
 #ifndef CLIENTFUNCTIONS_H
 #define CLIENTFUNCTIONS_H
 
+void clientRegister();//gets full name, id and password of the client and adds it to the data base
+
 
 #endif // !CLIENTFUNCTIONS_H
