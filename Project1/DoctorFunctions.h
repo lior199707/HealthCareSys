b8a270cd0f44359f1d9161b712ea783b97019793
noybuskila <noybu2@ac.sce.ac.il>
2021-11-23 22:59:46 +0200

    getMedicalField.h

--- Project1/DoctorFunctions.h
+++ Project1/DoctorFunctions.h
@@ -3,5 +3,7 @@
 #define DOCTORFUNCTIONS_H
 void doctorRegister();//gets the doctor full name, id,password,gender,medical field,title and the work hours (start hour and finish hour) and adds the doctor to the db
 void doctorLogIn();//gets the doctor id and password and checks if exists in the db
+char* getMedicalField();//gets the medical filed of the doctor from an existing list of medical fields
+
 
 #endif // !DOCTORFUNCTIONS_H
