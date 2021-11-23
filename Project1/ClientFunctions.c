8c72b57e625220c2f80ee16dc703fa67a7bf2431
noybuskila <noybu2@ac.sce.ac.il>
2021-11-23 21:47:19 +0200

    clientRegister.c

--- Project1/ClientFunctions.c
+++ Project1/ClientFunctions.c
@@ -3,5 +3,15 @@
 #include "MutualFunctions.h"
 #include "DoctorFunctions.h"
 
-
+//gets the client info and adds him to the database
+void clientRegister()
+{
+	char* fullName = getName();//gets the client full name
+	char* id = getId();//gets the client id
+	char* password = getPass();//gets the client password
+	addClientToDb(id, password, fullName);//adds him to the db
+	free(fullName);
+	free(id);
+	free(password);
+}
 
