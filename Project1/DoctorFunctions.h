8e6547fb05438334e07019bb25f7b8cad8afa931
Evgeni <bursov1995@gmail.com>
2021-11-23 21:47:37 +0200

    doctorRegister

--- Project1/DoctorFunctions.h
+++ Project1/DoctorFunctions.h
@@ -1,6 +1,7 @@
 #pragma once
 #ifndef DOCTORFUNCTIONS_H
 #define DOCTORFUNCTIONS_H
+void doctorRegister();//gets the doctor full name, id,password,gender,medical field,title and the work hours (start hour and finish hour) and adds the doctor to the db
 
 
 #endif // !DOCTORFUNCTIONS_H
