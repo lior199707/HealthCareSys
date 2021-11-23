5b44adc60746d8f6372de1c39e041935de8622d8
noybuskila <noybu2@ac.sce.ac.il>
2021-11-23 21:49:58 +0200

    chooseDateForAppointmnent.h

--- Project1/ClientFunctions.h
+++ Project1/ClientFunctions.h
@@ -3,6 +3,7 @@
 #define CLIENTFUNCTIONS_H
 
 void clientRegister();//gets full name, id and password of the client and adds it to the data base
+int chooseDateForAppointmnent(docId);//gets the date the client wants to schedule an appointment 
 
 
 #endif // !CLIENTFUNCTIONS_H
