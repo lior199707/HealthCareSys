2bf2cbd6bdc47b7e12e5517fda5e53e3930b36f8
noybuskila <noybu2@ac.sce.ac.il>
2021-11-23 21:53:42 +0200

    chooseDateForAppointmnent(docId).c

--- Project1/ClientFunctions.c
+++ Project1/ClientFunctions.c
@@ -14,4 +14,21 @@ void clientRegister()
 	free(id);
 	free(password);
 }
+//asks the client to choose a date for the appointment, prints to the screen a list of the doctors blocked dates so the client wont choose them
+int chooseDateForAppointmnent(docId)
+{
+	char* blockedDatesList = GetDetailsFromDb("blocked_dates", "doctorDb.db", "doctorInfo", docId);//gets the blocked dates list of the doctor by his id
+	puts("\nPlease choose the day for the appointment that doesnt appear in the doctor's blocked dates list\n");
+	printAllBlockedDates(blockedDatesList);//prints the blocked dates list ot the screen
+	int date = chooseDate();//asks the client to choose a date
+	//check if this date appears in the blocked dates list
+	while (isDateAppearInBlockDateList(blockedDatesList, date))//ass long it appears in the blockedl list
+	{
+		puts("The date you chose is blocked, please try again");
+		date = chooseDate();//asks for amother date from the client
+	}
+	free(blockedDatesList);
+	return date;
+}
+
 
