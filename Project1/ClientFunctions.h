9895e560d6018eb3148052bd6774e107badbdaff
shakedasido <shakedasido@gmail.com>
2021-11-26 03:17:20 +0200

    createTempAppointmentsList- function that gets the appointmnets list by doctor: id name gender, date & time and return a string. createTempAppointmentToCancle- function that gets as an input the doctor id,the date and the time of the appointment to cancel and returns it in this format:id*date*time .

--- Project1/ClientFunctions.h
+++ Project1/ClientFunctions.h
@@ -17,5 +17,6 @@ char* chooseDocIdByField(char* medicalField);//get the id of the doctor the clie
 char* createNewAllAppointmentsListByComa(char* AllAppointmentsList, int numOfComaToDel);//gets the future appoinmtnet list of the client and returns the list without the appointment to cancel by the index of the coma to delete
 int numOfComasForAppointmentCancelation(char* tempAllAppointmentsList, char* tempAppointmentToCancel);//return the number of comas has passed untill found the appointment to cancel,if wasnt found return -1
 char* chooseTimeForAppointment(char* docId, char* chosenDate, char* dateInDocDb);//get the time the client wants to schedule an appointment in
-
+char* createTempAppointmentsList(char* allAppointmentsList);//gets the appointmnets list by doctor: id name gender date time and return a string :id*date*time
+char* createTempAppointmentToCancle(char* docId, char* dateForAppointment, char* timeForAppointment);//gets as an input the doctor id,the date and the time of the appointment to cancel and returns it in this format:id*date*time  
 #endif // !CLIENTFUNCTIONS_H
