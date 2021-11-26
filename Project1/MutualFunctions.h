6611eac6a180b02ee2daf97ed5284b1d70803eda
Ohad Ezra <ohadez1@ac.sce.ac.il>
2021-11-26 13:37:21 +0200

    MutualFunctions

--- Project1/MutualFunctions.h
+++ Project1/MutualFunctions.h
@@ -11,9 +11,13 @@
  
 
 void startMenu();//this menu lets the user choose wether he is a client or a doctor
-
+char* getName();//gets the name of the user
+char* getId();//gets the id of the user
+char* getPass();//gets the password of the user
+int isNameValid(char* name);//checks if the name is valid, a valid name contains only letters and spaces  
+int isIdValid(char* id);//checks id the id is valid, a valid id contains exactly 9 digits and the first digit cant be 0
+int isPassValid(char* pass);//checks if the pass is valid, a valid pass contains at least: 1 upper, 1 lowwer and 1 digit and has at least 6 chars
 char* toString(const char* str);//returns a dynamical allocation of a char[] 
-
 char* detailsExistsInDb(const char* dbName, const char* id, const char* password);//checks if the id and password provided by the user exists in the db, used for log in
 void EditDetailsInDb(const char* what, const char* newWhat, const char* dbNmae, const char* tableName, const char* id);//gets what to edit, to what to edit it and where to edit it and edits it in the db
 char* GetDetailsFromDb(const char* what, const char* dbName, const char* tableName, const char* id);//gets what to get from the db and where to get it from and return a string of it
